bfe4d7916784081a2af217c692193d9f688408b8
Lucas Varas <lucas.varas@consorcio.cl>
2024-09-06 14:33:07 -0400

    Feat: initial proyecto configuration React/Typescript & Tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold text-blue-500">
+        Tailwind CSS está funcionando correctamente!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
